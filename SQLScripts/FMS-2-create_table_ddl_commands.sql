93ddf87a3555945b7ffe50495a66e7d283b50038
razvi786 <ehsan.razvi786@gmail.com>
2022-05-10 20:16:26 +0530

    Not Null Constraints added

--- SQLScripts/FMS-2-create_table_ddl_commands.sql
+++ SQLScripts/FMS-2-create_table_ddl_commands.sql
@@ -4,10 +4,10 @@ USE fms;
 
 CREATE TABLE IF NOT EXISTS fms.user(
 user_id integer NOT NULL AUTO_INCREMENT,
-role_id integer,
-name varchar(255),
-email varchar(255) UNIQUE,
-password varchar(255),
+role_id integer NOT NULL,
+name varchar(255) NOT NULL,
+email varchar(255) NOT NULL UNIQUE,
+password varchar(255) NOT NULL,
 phone varchar(50) UNIQUE,
 created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -17,8 +17,8 @@ CONSTRAINT fk_01_user_role FOREIGN KEY (role_id) references role(role_id));
 
 CREATE TABLE IF NOT EXISTS fms.project(
 project_id integer NOT NULL AUTO_INCREMENT,
-user_id integer,
-name varchar(255),
+user_id integer NOT NULL,
+name varchar(255) NOT NULL,
 description varchar(255),
 budget integer,
 created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -29,10 +29,10 @@ CONSTRAINT fk_01_project_user FOREIGN KEY (user_id) references user(user_id));
 
 CREATE TABLE IF NOT EXISTS fms.vendor(
 vendor_id integer NOT NULL AUTO_INCREMENT,
-account_holder_name varchar(255),
-account_number varchar(50),
-ifsc_code varchar(255),
-bank_name varchar(255),
+account_holder_name varchar(255) NOT NULL,
+account_number varchar(50) NOT NULL,
+ifsc_code varchar(255) NOT NULL,
+bank_name varchar(255) NOT NULL,
 branch varchar(255),
 upi_id varchar(255),
 created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -42,9 +42,9 @@ CONSTRAINT pk_vendor PRIMARY KEY (vendor_id));
 
 CREATE TABLE IF NOT EXISTS fms.approver_level(
 approver_level_id integer NOT NULL AUTO_INCREMENT,
-project_id integer,
-approver_id integer,
-level integer DEFAULT '1',
+project_id integer NOT NULL,
+approver_id integer NOT NULL,
+level integer NOT NULL DEFAULT '1',
 created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 concurrency_version integer NOT NULL,
