4315f9ee0b5f315758e398988e7b0d33cfdf560c
Syed Ehsan Ali Razvi <ehsan.razvi92@gmail.com>
2022-05-10 22:09:27 +0530

    Notification collection added

--- SQLScripts/create_collection_commands.sql
+++ SQLScripts/create_collection_commands.sql
@@ -4,3 +4,4 @@ use fms
 db.createCollection("payment", { autoIndexId : true })
 db.createCollection("request", { autoIndexId : true })
 db.createCollection("approval", { autoIndexId : true })
+db.createCollection("notification", { autoIndexId : true })
