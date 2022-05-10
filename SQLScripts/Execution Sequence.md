624a81d4b125552d82eaa98e9b4c1305773bc58a
vaishnavi-bale <59390707+vaishnavi-bale@users.noreply.github.com>
2022-05-10 21:36:57 +0530

    SQL scripts (#1)
    * SQL scripts
    
    * sql scripts for rest of the tables
    
    * Scripts executed and tested
    
    Co-authored-by: razvi786 <ehsan.razvi786@gmail.com>

--- SQLScripts/Execution Sequence.md
+++ SQLScripts/Execution Sequence.md
@@ -1,11 +1,11 @@
 # MySQL Scripts
 
-1. FMS-1-create_database_command.sql
-2. FMS-2-create_table_ddl_commands.sql
+1. create_database_command.sql
+2. create_table_ddl_commands.sql
 
 # MongoDB Scripts
 
-1. FMS-1-create_database_command.sql
-2. FMS-5-create_collection_commands.sql
+1. create_database_command.sql
+2. create_collection_commands.sql
 
 > **Note:** Only Execute particular part of Script either SQL Scripts or MongoDB Scripts
--- SQLScripts/create_table_ddl_commands.sql
+++ SQLScripts/create_table_ddl_commands.sql
@@ -2,6 +2,35 @@
 
 USE fms;
 
+SET SQL_MODE='ALLOW_INVALID_DATES';
+
+CREATE TABLE IF NOT EXISTS fms.permission(
+permission_id integer NOT NULL AUTO_INCREMENT,
+name varchar(100) NOT NULL,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+concurrency_version integer NOT NULL,
+CONSTRAINT pk_permission PRIMARY KEY (permission_id));
+
+CREATE TABLE IF NOT EXISTS fms.role(
+role_id integer NOT NULL AUTO_INCREMENT,
+name varchar(100) NOT NULL,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+concurrency_version integer NOT NULL,
+CONSTRAINT pk_role PRIMARY KEY (role_id));
+
+CREATE TABLE IF NOT EXISTS fms.role_permission(
+role_permission_id integer NOT NULL AUTO_INCREMENT,
+role_id integer NOT NULL,
+permission_id integer NOT NULL,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+concurrency_version integer NOT NULL,
+CONSTRAINT pk_role_permission PRIMARY KEY (role_permission_id),
+CONSTRAINT fk_01_role_permission FOREIGN KEY (role_id) references role(role_id),
+CONSTRAINT fk_02_role_permission FOREIGN KEY (permission_id) references permission(permission_id));
+
 CREATE TABLE IF NOT EXISTS fms.user(
 user_id integer NOT NULL AUTO_INCREMENT,
 role_id integer NOT NULL,
@@ -51,3 +80,19 @@ concurrency_version integer NOT NULL,
 CONSTRAINT pk_approver_level PRIMARY KEY (approver_level_id),
 CONSTRAINT fk_01_approver_level_project FOREIGN KEY (project_id) references project(project_id),
 CONSTRAINT fk_02_approver_level_user FOREIGN KEY (approver_id) references user(user_id));
+
+CREATE TABLE IF NOT EXISTS fms.notification(
+notification_id integer NOT NULL AUTO_INCREMENT,
+notification_type ENUM('AUTOMATED', 'MANUAL'),
+message varchar(255),
+triggered_by integer NOT NULL,
+triggered_to integer NOT NULL,
+seen_datetime timestamp,
+sms_sent_datetime timestamp,
+email_sent_datetime timestamp,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+concurrency_version integer NOT NULL,
+CONSTRAINT pk_notification PRIMARY KEY (notification_id),
+CONSTRAINT fk_01_user_notification FOREIGN KEY (triggered_by) references user(user_id),
+CONSTRAINT fk_02_user_notification FOREIGN KEY (triggered_to) references user(user_id));
