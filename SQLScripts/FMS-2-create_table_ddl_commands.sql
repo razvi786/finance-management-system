c4c826ec3200555c0aa9fce6c2276855b73190a3
razvi786 <ehsan.razvi786@gmail.com>
2022-05-10 19:05:51 +0530

    Scripts updated after executing them

--- SQLScripts/FMS-2-create_table_ddl_commands.sql
+++ SQLScripts/FMS-2-create_table_ddl_commands.sql
@@ -2,34 +2,32 @@
 
 USE fms;
 
-create table user(
+CREATE TABLE IF NOT EXISTS fms.user(
 user_id integer NOT NULL AUTO_INCREMENT,
 role_id integer,
 name varchar(255),
 email varchar(255) UNIQUE,
 password varchar(255),
 phone varchar(50) UNIQUE,
-created_datetime timestampz NOT NULL,
-updated_datetime timestampz,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 concurrency_version integer NOT NULL,
 CONSTRAINT pk_user PRIMARY KEY (user_id),
 CONSTRAINT fk_01_user_role FOREIGN KEY (role_id) references role(role_id));
 
-create table project(
+CREATE TABLE IF NOT EXISTS fms.project(
 project_id integer NOT NULL AUTO_INCREMENT,
 user_id integer,
-approver_level_id integer,
 name varchar(255),
 description varchar(255),
 budget integer,
-created_datetime timestampz NOT NULL,
-updated_datetime timestampz,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 concurrency_version integer NOT NULL,
 CONSTRAINT pk_project PRIMARY KEY (project_id),
-CONSTRAINT fk_01_project_user FOREIGN KEY (user_id) references user(user_id),
-CONSTRAINT fk_02_project_approver_level FOREIGN KEY (approver_level_id) references approver_level(approver_level_id));
+CONSTRAINT fk_01_project_user FOREIGN KEY (user_id) references user(user_id));
 
-create table vendor(
+CREATE TABLE IF NOT EXISTS fms.vendor(
 vendor_id integer NOT NULL AUTO_INCREMENT,
 account_holder_name varchar(255),
 account_number varchar(50),
@@ -37,18 +35,18 @@ ifsc_code varchar(255),
 bank_name varchar(255),
 branch varchar(255),
 upi_id varchar(255),
-created_datetime timestampz NOT NULL,
-updated_datetime timestampz,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 concurrency_version integer NOT NULL,
 CONSTRAINT pk_vendor PRIMARY KEY (vendor_id));
 
-create table approver_level(
+CREATE TABLE IF NOT EXISTS fms.approver_level(
 approver_level_id integer NOT NULL AUTO_INCREMENT,
 project_id integer,
 approver_id integer,
 level integer DEFAULT '1',
-created_datetime timestampz NOT NULL,
-updated_datetime timestampz,
+created_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+updated_datetime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 concurrency_version integer NOT NULL,
 CONSTRAINT pk_approver_level PRIMARY KEY (approver_level_id),
 CONSTRAINT fk_01_approver_level_project FOREIGN KEY (project_id) references project(project_id),
